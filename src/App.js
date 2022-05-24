d7a855ace6e7b590867b7b7f9efc28c892e9b04d
Thomas-Af <thomas.afonso@mail-ecv.fr>
2022-05-24 13:03:51 +0200

    Add api and fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+ const [data, setData] = useState(null);
+ const [loading, setLoading] = useState(true);
+ const [error, setError] = useState(null);
+
+ useEffect(() => {
+  fetch("http://api.openeventdatabase.org/event?what=sport.competition.olympics&start=20160803&stop=20160822&limit=3500")
+  .then(function (maReponse) {
+    console.log("maReponse", maReponse); 
+
+    return maReponse.json();
+  })
+  .then(function (maDonnee) {
+    console.log("maDonnee", maDonnee); 
+    console.log("Sports Name", maDonnee.features); 
+
+    maDonnee.features.forEach(element => {
+      console.log('Nom', element.properties.name)
+    });
+  });
+ }, []);
+
+
+ 
+
+ return <div className="App">App</div>;
 }
-
-export default App;
